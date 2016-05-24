7bb5b94bcc8641b1e3bed1ce9311c42a23d48a9c
Moon_C <cms647218@gmail.com>
2016-05-24 14:56:07 -0400

    added weapon ui

--- scripts/renderUI.js
+++ scripts/renderUI.js
@@ -16,4 +16,29 @@ function render_UI()
 	surface.font = "30px Verdana";
 	surface.fillText("Lives: ", 50, 50);
 	surface.fillText(playerLives, 200, 50);
+	
+	// Render Weapon UI
+		surface.font = "20px Verdana";
+		surface.fillText("1", 50,525);
+				surface.fillText("2", 100,525);
+		surface.fillText("3", 150,525);
+		surface.beginPath();
+		switch (player.current_weapon)
+		{
+			case 1:
+			surface.rect(50,500,25,25);
+			break;
+			case 2:
+			surface.rect(100,500,25,25);
+			break;
+			case 3:
+			surface.rect(150,500,25,25);
+			break;
+			default:
+			console.log("No weapon");
+			break;
+			
+		}
+		surface.stroke();
+		
 }
